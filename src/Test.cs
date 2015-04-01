cd0e6ec15b32c9a9a9f4464e2526a05bc4f0bc94
Pete Smith <roysvork@gmail.com>
2015-04-01 09:56:53 +0100

    Initial project files and travis config

---
+++ src/Test.cs
@@ -0,0 +1,98 @@
+﻿namespace testingbotdemo
+{
+    using System;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using OpenQA.Selenium.Remote;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        private TestContext testContextInstance;
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return this.testContextInstance;
+            }
+            set
+            {
+                this.testContextInstance = value;
+            }
+        }
+
+
+        private TestingBotDriver driver;
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            String sessionId = this.driver.getSessionId();
+            bool success = this.TestContext.CurrentTestOutcome == UnitTestOutcome.Passed;
+            try
+            {
+                this.driver.updateTestStatus(sessionId, success, this.TestContext.FullyQualifiedTestClassName + " " + DateTime.Now.ToLongTimeString() + " " + this.TestContext.CurrentTestOutcome.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                this.driver.Quit();
+            }
+        }
+
+        /// <param name="cap">TestObj</param>
+        /// <param name="version">browser version</param>
+        /// <param name="platform">OS</param>
+        /// <returns></returns>
+        private DesiredCapabilities GetTestObj(DesiredCapabilities cap, string version, string platform)
+        {
+            cap.SetCapability(CapabilityType.Platform, platform);
+            cap.SetCapability(CapabilityType.Version, version);
+            cap.IsJavaScriptEnabled = true;
+            cap.SetCapability("screenshot", true);
+            cap.SetCapability("name", "test name");
+            cap.SetCapability("api_key", TestingBotDriver.APIKEY);
+            cap.SetCapability("api_secret", TestingBotDriver.APISECRET);
+
+            return cap;
+        }
+
+        /// <param name="cap"></param>
+        private void DoTitleTest(DesiredCapabilities cap)
+        {
+            this.driver = new TestingBotDriver(new Uri("http://hub.testingbot.com:80/wd/hub/"), cap);
+            this.driver.Navigate().GoToUrl("http://www.google.com");
+
+            StringAssert.Equals(this.driver.Title, "Google");
+        }
+
+        [TestMethod]
+        public void Win_InternetExplorer_7()
+        {
+            DesiredCapabilities cap = this.GetTestObj(DesiredCapabilities.InternetExplorer(), "7", "WINDOWS");
+            this.DoTitleTest(cap);
+        }
+
+        [TestMethod]
+        public void Win_InternetExplorer_8()
+        {
+            DesiredCapabilities cap = this.GetTestObj(DesiredCapabilities.InternetExplorer(), "8", "WINDOWS");
+            this.DoTitleTest(cap);
+        }
+
+        [TestMethod]
+        public void Win_InternetExplorer_9()
+        {
+            DesiredCapabilities cap = this.GetTestObj(DesiredCapabilities.InternetExplorer(), "9", "WINDOWS");
+            this.DoTitleTest(cap);
+        }
+    }
+}
---
+++ src/TestingDriver.cs
@@ -0,0 +1,63 @@
+﻿//using Selenium;  
+
+namespace testingbotdemo
+{
+    using System;
+    using System.IO;
+    using System.Net;
+    using System.Text;
+
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Remote;
+
+    public class TestingBotDriver : RemoteWebDriver
+    {
+
+        public static string APIKEY = "key";
+        public static string APISECRET = "secret";
+
+        public TestingBotDriver(ICommandExecutor commandExecutor, ICapabilities desiredCapabilities)
+            : base(commandExecutor, desiredCapabilities)
+        {
+        }
+
+        public TestingBotDriver(ICapabilities desiredCapabilities)
+            : base(new Uri("http://hub.testingbot.com:80/wd/hub"), desiredCapabilities)
+        {
+        }
+
+        public TestingBotDriver(Uri remoteAddress, ICapabilities desiredCapabilities)
+            : base(remoteAddress, desiredCapabilities, TimeSpan.FromSeconds(120))
+        {
+        }
+
+        public String getSessionId()
+        {
+            return this.SessionId.ToString();
+        }
+
+        public void updateTestStatus(String sessionId, bool success, String testName)
+        {
+            String url = "http://api.testingbot.com/v1/tests/" + sessionId;
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url);
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.Method = "PUT";
+            string username = APIKEY;
+            string password = APISECRET;
+
+            string usernamePassword = username + ":" + password;
+            CredentialCache mycache = new CredentialCache();
+            mycache.Add(new Uri(url), "Basic", new NetworkCredential(username, password));
+            request.Credentials = mycache;
+            request.Headers.Add("Authorization", "Basic " + Convert.ToBase64String(new ASCIIEncoding().GetBytes(usernamePassword)));
+
+            using (StreamWriter writer = new StreamWriter(request.GetRequestStream()))
+            {
+                writer.Write("test[success]=" + (success ? "1" : "0") + "&test[name]=" + testName);
+            }
+
+            WebResponse response = request.GetResponse();
+            response.Close();
+        }
+    }
+}
